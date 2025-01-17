b67cba33b88093da2dba3440a9cfc532fd5c653c
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-01-17 13:22:17 +0100

    refactor: clean up Calculator contract and simplify add function

--- lesson1.sol
+++ lesson1.sol
@@ -1,10 +1,7 @@
 pragma solidity ^0.8.0;
 
-contract Calculator{
+contract Calculator {
     uint256 result = 0;
 
-    function add() public view return s {
-
-    }
-    
-} 
+    function add() public view {}
+}
