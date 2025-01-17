4627ef58a2175983994c3a2c31c0524dbf87d051
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-01-17 20:06:26 +0100

    calculator done (lesson1)

--- lesson1.sol
+++ lesson1.sol
@@ -3,5 +3,19 @@ pragma solidity ^0.8.0;
 contract Calculator {
     uint256 result = 0;
 
-    function add() public view {}
+    function add(uint256 num) public {
+        result += num;
+    }
+
+    function sub(uint256 num) public {
+        result -= num;
+    }
+
+    function mul(uint256 num) public {
+        result *= num;
+    }
+
+    function get() public view returns (uint256) {
+        return result;
+    }
 }
