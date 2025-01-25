4ccc8ea7b9eac0ab2cfb77951d7ca609a74401a3
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-01-25 19:05:22 +0100

    Array created and done

--- lesson3.sol
+++ lesson3.sol
@@ -1,1 +1,9 @@
-//Array
+//Arrays
+contract Array{
+    uint[] public dynamicArray;
+
+    function addToDynamicArray{uint value} public{
+        dynamicArray.push(value); //pushes value into the array 
+    }
+    uint[10] public fixedArray; //initialise the array with a fixed size
+}
