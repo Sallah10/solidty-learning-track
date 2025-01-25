5f8ebf80c6601bab17cc4bcc427a4b10ec49ae0b
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-01-25 19:36:35 +0100

    struct added to lesson 3

--- lesson3.sol
+++ lesson3.sol
@@ -1,4 +1,4 @@
-//Arrays
+//Arrays and Struct
 contract Array{
     uint[] public dynamicArray;
 
@@ -6,4 +6,15 @@ contract Array{
         dynamicArray.push(value); //pushes value into the array 
     }
     uint[10] public fixedArray; //initialise the array with a fixed size
+
+    mapping(address => string[]) public mappedArray;
+}
+`
+contract Struct{
+    struct Course{
+        string courseName;
+        uint16 courseCode;
+        string courseLecture
+    }
+    Course[] public courses;
 }
