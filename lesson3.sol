88614efd50514f86d801944f8229cd495f7c30e1
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-01-27 17:16:23 +0100

    fix: correct syntax in addToDynamicArray function

--- lesson3.sol
+++ lesson3.sol
@@ -2,7 +2,7 @@
 contract Array{
     uint[] public dynamicArray;
 
-    function addToDynamicArray{uint value} public{
+    function addToDynamicArray(uint value) public{
         dynamicArray.push(value); //pushes value into the array 
     }
     uint[10] public fixedArray; //initialise the array with a fixed size
