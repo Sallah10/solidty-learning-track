2ad08533f5a652705d9594808cce00ad5367bc10
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-02-01 07:39:08 +0100

    lesson4 corrected

--- lesoon4.sol
+++ lesoon4.sol
@@ -4,6 +4,11 @@ pragma solidity ^0.8.0;
 contract Twitter {
     mapping(address => string) public tweets;
     function reduceLength(string memory _tweet) public {
-        require(len(_tweet) <= 200, "length is more than 200 lengths");
+        uint128 constant max_tweet_length = 280
+
+        require(
+            byte(_tweet).length <= max_tweet_length,
+            "Tweet is too long more than 200 characters"
+        );
     }
 }
