300ffc72cb1b90e1747d8adf1570cf8fbc96272f
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-02-02 21:39:00 +0100

    modifier sample added

--- lesoon4.sol
+++ lesoon4.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 contract Twitter {
-    mapping(address => string) public tweets;
     function reduceLength(string memory _tweet) public {
         uint128 constant max_tweet_length = 280
 
@@ -10,5 +9,9 @@ contract Twitter {
             byte(_tweet).length <= max_tweet_length,
             "Tweet is too long more than 200 characters"
         );
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender === owner, "You are not the Owner");
     }
 }
